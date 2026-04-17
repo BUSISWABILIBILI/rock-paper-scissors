d41e19924057a929ec3e2d4c378524d27742e425
Busiswa Bili-bili <141039125+BUSISWABILIBILI@users.noreply.github.com>
2026-04-17 12:57:29 +0200

    Add playRound function with scoring system

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
 
-  if (randomNumber === 0) {
-    return "rock";
-  } else if (randomNumber === 1) {
-    return "paper";
-  } else {
-    return "scissors";
-  }
+  if (randomNumber === 0) return "rock";
+  if (randomNumber === 1) return "paper";
+  return "scissors";
 }
 
 function getHumanChoice() {
   return prompt("Enter rock, paper, or scissors:");
 }
 
-console.log(getHumanChoice());
-console.log(getComputerChoice());
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
 
-let humanScore = 0;
-let computerScore = 0;
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    humanScore++;
+    console.log("You win! " + humanChoice + " beats " + computerChoice);
+  } else {
+    computerScore++;
+    console.log("You lose! " + computerChoice + " beats " + humanChoice);
+  }
+
+  console.log("Human: " + humanScore);
+  console.log("Computer: " + computerScore);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
